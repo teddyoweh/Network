8549088186eeb0990cb86c84d482307e4a409664
Ifechukwudeni Oweh <76883580+teddyoweh@users.noreply.github.com>
2022-05-08 12:57:53 -0500

    update

---
+++ network/src/Components/Banner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+ 
+
+export default class Banner extends Component {
+
+  render() {
+    return(
+      <div className="Banner">
+       
+        <p>Click on an emoji to view the emoji short name.</p>
+      </div>
+    )
+  }
+}
