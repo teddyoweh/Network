895ba890f967c15d486f20895ad685fed1d0b273
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-05-12 15:23:43 +0100

    Excluded React Router Module, Since Not Declared.

--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -1,29 +1,18 @@
- 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
- 
-import { HomeComponent } from '../home';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { SidePanelSection } from '../sidepanel';
- 
 const MainComponent = () => {
-    return (
-        <Router> 
-            <Switch> 
-                <Route path ="/" exact >
-                    <>
-                        <SidePanelSection />
-                        <main id="main">
-                            <HomeComponent />      
-                        </main>
-                        
-                    </>
-                </Route>
-
-                 
-
-            </Switch>   
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <>
+            <main id="main">
+            </main>
+          </>
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default MainComponent;
