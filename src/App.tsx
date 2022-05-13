7681ce1b2420dd8aac55981d98583722f0d70c9d
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-05-13 11:55:24 +0100

    Fix, Implement Index.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import Navigation from './components/Navigation';
+function App() {
+  return (
+    <>
+    <Navigation />
+    <div>Hello There</div>
+    </>
+  );
+}
+
+export default App;
