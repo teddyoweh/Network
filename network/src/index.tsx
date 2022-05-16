1f4184a64634266fa4ae8037041c5827a09b8b4b
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-05-16 11:34:18 +0100

    Add Browser Router.

--- network/src/index.tsx
+++ network/src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, RouteComponentProps  } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Discover from './routes/Discover';
+import Colleges from './routes/Colleges';
+import Friends from './routes/Friends';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Route>
+      <Route path="/" element={<App />} />
+      <Route path="Discover" element={<Discover />} />
+      <Route path="Friends" element={<Friends />} />
+      <Route path="Colleges" element={<Colleges />} />
+    </Route>
+  </BrowserRouter>
 );
