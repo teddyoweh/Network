136a98fb677bcb76b46f3dd480834cb8b1b7cb46
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-05-24 10:02:38 -0500

    Update routes.component.js

--- network/src/components/routes/routes.component.js
+++ network/src/components/routes/routes.component.js
@@ -1,46 +1,21 @@
-<<<<<<< HEAD:src/components/main/main.component.tsx
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+ import { HomeComponent } from '../home';
 
-const MainComponent = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <>
-            <main id="main">
-            </main>
-          </>
-        </Route>
-      </Switch>
+ const RouteComponent = () => {
+     return ( 
+     <Router>
+         <Switch>
+         <Route path = "/"exact >
+         <>
+
+         <main id = "main" >
+         <HomeComponent/>
+         </main>
+
+         </> </Route>
+
+
+         </Switch>    
     </Router>
-  );
-};
-
-export default MainComponent;
-=======
- 
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import { HomeComponent } from '../home';
-
-const RouteComponent = () => {
-    return (
-        <Router> 
-            <Switch> 
-                <Route path ="/" exact >
-                    <>
-                        
-                        <main id="main">
-                            <HomeComponent />      
-                        </main>
-                        
-                    </>
-                </Route>
-  
-
-            </Switch>   
-        </Router>
-    );
-}
-
-export default RouteComponent;
->>>>>>> 17a1ede4728bf55474d7034742c4ca7b47ab771f:network/src/components/routes/routes.component.js
+     );
+ }
