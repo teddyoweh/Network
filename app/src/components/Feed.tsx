a5084057f04637899b44bd7d8a493fe2cf529283
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2022-06-02 16:11:27 -0500

    Update Feed.tsx

--- app/src/components/Feed.tsx
+++ app/src/components/Feed.tsx
@@ -1,7 +1,8 @@
+ 
 import React  from "react";
 import "./assets/css/feed.css";
 import Image from "./assets/img.png";
-import Avatar from "./Avatar";
+import Post from './Post';
 import 'boxicons';
 function Feed(){
     return(
@@ -13,25 +14,10 @@ function Feed(){
   href="https://unpkg.com/boxicons@latest/css/boxicons.min.css"></link>
         <div className="container-fluid" id='feed'>
         <ul className="list-group">
-  <li className="list-group-item">
-  <figure className="figure">
-   
-  <figcaption className="figure-caption">A caption for the above image.</figcaption>
-  <a href="#" className="btn btn-primary"><i className='bx bx-message-rounded-dots'></i>  Comment</a>
-</figure>
-    
-    </li>
-    <li className="list-group-item">
-  <figure className="figure">
-  <img src={Image} className="figure-img img-fluid rounded" alt="..."/>
-  <figcaption className="figure-caption">A caption for the above image.</figcaption>
-  <div className='post-insight'> 
-  <a href="#" className="btn btn-primary"><i className='bx bx-message-rounded-dots'></i>  Comment</a>
-  <Avatar></Avatar>
-  </div>
-</figure>
-    
-    </li>
+  
+    <Post></Post>
+    <Post></Post>
+    <Post></Post>
    
 </ul>
  
